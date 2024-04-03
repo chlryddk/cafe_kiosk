31107fa1adc4280228f77af9f8d3a3748501fb31
ryddk <ryddk0301@gmail.com>
2024-04-03 18:22:07 +0900

    create db table

---
+++ kiosk/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.category_name
+    
+class Menu(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    menu_name = models.CharField(max_length=40, unique=True)
+    price = models.IntegerField(default='0')
+
+    def __str__(self):
+        return self.menu_name
+    
+class Customer(models.Model):
+    customer_name = models.CharField(max_length=100)
+
+class Order(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    count = models.IntegerField(default=0)
+    date = models.DateTimeField(auto_now_add=True)
